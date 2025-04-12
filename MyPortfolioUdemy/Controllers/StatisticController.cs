c31365740642d7b5e073edb3aa885f4b8d90443d
BERK <berkdemirbozan@hotmail.com>
2025-04-12 15:45:04 +0300

    Add project files.

---
+++ MyPortfolioUdemy/Controllers/StatisticController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioUdemy.DAL.Context;
+
+namespace MyPortfolioUdemy.Controllers
+{
+    public class StatisticController : Controller
+    {
+        MyPortfolioContext context = new MyPortfolioContext();
+        public IActionResult Index()
+        {
+            ViewBag.v1 = context.Skills.Count(); //yetenek sayısı
+            ViewBag.v2 = context.Messages.Count(); // toplam mesaj
+            ViewBag.v3 = context.Messages.Where(x=>x.IsRead == false).Count(); //okunmayan mesaj
+            ViewBag.v4 = context.Messages.Where(x=>x.IsRead == true).Count(); //okunan mesaj
+            return View();
+        }
+    }
+}
